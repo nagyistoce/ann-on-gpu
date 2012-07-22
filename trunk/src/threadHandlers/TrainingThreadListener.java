ef01a67797fa118be2d6d73cbf609106ef436cec
praveen@spikingneurons.com <praveen@spikingneurons.com@a2f56e65-c81a-b062-1a98-2de5f028b4da>
2012-07-22 12:49:59 +0000

    adding author information to all files

--- trunk/src/threadHandlers/TrainingThreadListener.java
+++ trunk/src/threadHandlers/TrainingThreadListener.java
@@ -4,7 +4,7 @@ package threadHandlers;
  * This interface contains four methods whose implementation will be executed when
  * postMessage() method of JavaTrainingThreadTask is executed.
  * These methods are responsible for update of UI
- * @author praveen_kulkarni02
+ * @author praveen_kulkarni
  */
 public interface TrainingThreadListener {
     public void communicateIterationsCompleted(Integer iterationsCompleted);
